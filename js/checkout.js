32218f2df73ed3846067941ee4198add12ac45ee
tjong1 <47622135+tjong1@users.noreply.github.com>
2022-04-13 08:34:04 +0800

    more changes to checkout

--- js/checkout.js
+++ js/checkout.js
@@ -3,10 +3,35 @@ function deliveryMethod(selected){
         case "standard":
             document.getElementById("shippingAmount").innerHTML = "FREE";
             document.getElementById("shippingAmount").style.color = "#FF740F";
+            calcTotal();
             break;
         case "express":
             document.getElementById("shippingAmount").innerHTML = "RM20.00";
-            document.getElementById("shippingAmount").style.color = "#00000091";
+            document.getElementById("shippingAmount").style.color = "#000000";
+            calcTotal();
             break;
     }
+}
+
+function calcTax() {
+    var total = document.getElementById("itemSubtotal").innerHTML;
+    var tax = (total * 0.06).toFixed(2);
+    document.getElementById("taxAmount").innerHTML = tax;
+}
+
+function calcTotal() {
+    var total = document.getElementById("itemSubtotal").innerHTML;
+    var tax = document.getElementById("taxAmount").innerHTML;
+    var shipping = 0;
+    var shippingMethod = document.getElementById("shippingAmount").innerHTML; 
+    switch (shippingMethod) {
+        case "FREE":
+            shipping = 0;
+            break;
+        case "RM20.00":
+            shipping = 20;
+            break;
+    }
+    var totalAmount = (parseFloat(total) + parseFloat(tax) + parseFloat(shipping)).toFixed(2);
+    document.getElementById("totalAmount").innerHTML = "RM" + totalAmount;
 }
